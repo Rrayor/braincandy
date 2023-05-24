8b7a870ad275bf801a4284607780f481429dc7f1
Rrayor <rrayor.dev@gmail.com>
2023-05-24 22:10:12 +0200

    chore: :wrench: wip: Nest config Prisma
    configured db with prisma, added basic schema

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,41 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { DataSet, Prisma } from '@prisma/client';
 import { AppService } from './app.service';
+import { PrismaService } from './modules/shared/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly prismaService: PrismaService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  //TODO: remove test code
+  @Get('/create')
+  async createDataSet(): Promise<DataSet> {
+    const data: Prisma.DataSetCreateInput = {
+      handle: 'handle',
+      entries: [
+        {
+          key: 'key1',
+          value: 'value1',
+        },
+        {
+          key: 'key2',
+          value: 'value2',
+        }
+      ],
+    };
+    return this.prismaService.dataSet.create({
+      data
+    })
+  }
+
+  @Get('/getall')
+  async getAllDataSets(): Promise<Array<DataSet>> {
+    return this.prismaService.dataSet.findMany();
   }
 }
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './modules/shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        DATABASE_URL: Joi.string().required()
+      }),
+      validationOptions: {
+        allowUnknown: true
+      }
+    }),
+    SharedModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
