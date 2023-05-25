87b60c9f759847a8e550445258098d9a9009fcc0
Rrayor <rrayor.dev@gmail.com>
2023-05-25 19:42:30 +0200

    chore: :coffin: removed test code

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { DataSet, Prisma } from '@prisma/client';
 import { AppService } from './app.service';
 import { PrismaService } from './modules/shared/prisma.service';
 
@@ -11,31 +10,5 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
-  }
-
-  //TODO: remove test code
-  @Get('/create')
-  async createDataSet(): Promise<DataSet> {
-    const data: Prisma.DataSetCreateInput = {
-      handle: 'handle',
-      entries: [
-        {
-          key: 'key1',
-          value: 'value1',
-        },
-        {
-          key: 'key2',
-          value: 'value2',
-        }
-      ],
-    };
-    return this.prismaService.dataSet.create({
-      data
-    })
-  }
-
-  @Get('/getall')
-  async getAllDataSets(): Promise<Array<DataSet>> {
-    return this.prismaService.dataSet.findMany();
   }
 }
