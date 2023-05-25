dda0f98bf782a64656920df1c0f95dd93d3b854b
Rrayor <rrayor.dev@gmail.com>
2023-05-25 20:01:34 +0200

    Chore/nest config (#2)
    * chore: :wrench: wip: Nest config Prisma
    
    configured db with prisma, added basic schema
    
    * chore: :heavy_plus_sign: nest configuration
    
    Mostly configured NestJS application:
    
    * fix: :bug: csrf fix
    
    added csrf key to env, so csrf protection works
    
    * chore: :coffin: removed test code
    
    * fix: :green_heart: nx format config fix
    
    changed nx base from master to main
    
    * chore: :art: formatting
    
    ran nx format:write

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,41 @@
 import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './modules/shared/shared.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        DATABASE_URL: Joi.string().required(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+      },
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 3_600_000,
+      max: 100,
+    }),
+    SharedModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,16 +3,44 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import csurf from 'tiny-csrf';
 
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  app.use(helmet());
+  app.use(csurf(configService.get<string>('CSRF_KEY')));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Braincandy')
+    .setDescription('Generic data visualization')
+    .setVersion('1.0')
+    .addTag('Data')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
