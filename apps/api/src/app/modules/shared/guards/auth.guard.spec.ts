752f3c2f179f09c19a2c3f23597a829899cb7962
Rrayor <rrayor.dev@gmail.com>
2023-05-30 09:00:56 +0200

    test: :white_check_mark: wip: auth guard test

--- apps/api/src/app/modules/shared/guards/auth.guard.spec.ts
+++ apps/api/src/app/modules/shared/guards/auth.guard.spec.ts
@@ -1,20 +1,24 @@
-import { Logger } from "@nestjs/common";
-import { Test, TestingModule } from "@nestjs/testing";
-import { FireBaseAdmin } from "../firebase.setup";
-import { AuthGuard } from "./auth.guard";
+import { Logger } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { FireBaseAdmin } from '../firebase.setup';
+import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
-    let guard: AuthGuard;
+  let guard: AuthGuard;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [{ provide: FireBaseAdmin, useValue: {} }, { provide: Logger, useValue: {} }, AuthGuard]
-        }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        { provide: FireBaseAdmin, useValue: {} },
+        { provide: Logger, useValue: {} },
+        AuthGuard,
+      ],
+    }).compile();
 
-        guard = module.get<AuthGuard>(AuthGuard);
-    });
+    guard = module.get<AuthGuard>(AuthGuard);
+  });
 
-    it('should be defined', () => {
-        expect(guard).toBeDefined();
-    });
+  it('should be defined', () => {
+    expect(guard).toBeDefined();
+  });
 });
