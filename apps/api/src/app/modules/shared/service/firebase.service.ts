3a4bcb4ad8e3007c6f8c1eb94035952f4239a723
Rrayor <rrayor.dev@gmail.com>
2023-07-09 13:11:52 +0200

    chore: :bulb: extended TODO with ticket number

--- apps/api/src/app/modules/shared/service/firebase.service.ts
+++ apps/api/src/app/modules/shared/service/firebase.service.ts
@@ -4,7 +4,7 @@ import { Roles } from '../../auth/enums/roles.enum';
 
 @Injectable()
 export class FireBaseService {
-  // TODO: should refactor everything so FirebaseAdmin doesn't need to be used anywhere. This Service should do everything related to Firebase
+  // TODO: should refactor everything so FirebaseAdmin doesn't need to be used anywhere. This Service should do everything related to Firebase - #9
   async verifyToken(
     app: admin.app.App,
     idToken: string
